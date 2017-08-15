69ef4f2aa5548fbc6cbdaea59e4782b2c294c02f
swatghc <447099673@qq.com>
2017-08-15 21:29:13 +1000

    666

--- app/src/main/java/com/example/eurka/comp90018/MainActivity.java
+++ app/src/main/java/com/example/eurka/comp90018/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    //this is a comment to test the github
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
